b9b0c5df9aa904f59dedec2d8feeff8988768288
ikuki <akedaikuki@gmail.com>
2023-05-12 13:46:07 +0800

    chore: heroku init

--- app.js
+++ app.js
@@ -147,6 +147,16 @@ app.use(routes)
 
 
 // 設定 port 3000
-app.listen(3000, () => {
-  console.log('App is running on http://localhost:3000')
+// app.listen(3000, () => {
+//   console.log('App is running on http://localhost:3000')
+// })
+
+// 修改環境設定
+// app.js
+// 如果在 Heroku 環境則使用 process.env.PORT
+// 否則為本地環境，使用 3000 
+const PORT = process.env.PORT || 3000
+// 設定應用程式監聽的埠號
+app.listen(PORT, () => {
+  console.log(`App is running on http://localhost:${PORT}`)
 })
