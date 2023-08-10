7a018a571b598a24c0027cfba6a3389d38261b78
ikuki <akedaikuki@gmail.com>
2023-08-10 15:43:10 +0800

    feat: add logout feature

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -34,31 +34,35 @@ router.post("/register", (req, res) => {
   // 取得註冊表單參數
   const { name, email, password, confirmPassword } = req.body;
   // 檢查使用者是否已經註冊
-  User.findOne({ email })
-    .then((user) => {
-      // 如果已經註冊：退回原本畫面
-      if (user) {
-        console.log("User already exists.");
-        res.render("register", {
-          // 再附上表單參數
-          name,
-          email,
-          password,
-          confirmPassword,
-        });
-      } else {
-        // 新使用者：創建資料
-        // 如果還沒註冊：寫入資料庫
-        return User.create({
-          name,
-          email,
-          password,
-        })
-          .then(() => res.redirect("/"))
-          .catch((err) => console.log(err));
-      }
-    })
-    .catch((err) => console.log(err));
+  User.findOne({ email }).then((user) => {
+    // 如果已經註冊：退回原本畫面
+    if (user) {
+      console.log("User already exists.");
+      res.render("register", {
+        // 再附上表單參數
+        name,
+        email,
+        password,
+        confirmPassword,
+      });
+    } else {
+      // 新使用者：創建資料
+      // 如果還沒註冊：寫入資料庫
+      return User.create({
+        name,
+        email,
+        password,
+      })
+        .then(() => res.redirect("/"))
+        .catch((err) => console.log(err));
+    }
+  });
+});
+
+// 新增登出路由
+router.get("/logout", (req, res) => {
+  req.logout();
+  res.redirect("/users/login");
 });
 
 // 匯出路由模組
