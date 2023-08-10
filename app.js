6f5e8965034bb9735d0d05abb02b595f04145327
ikuki <akedaikuki@gmail.com>
2023-08-10 11:15:39 +0800

    feat: add express-session

--- app.js
+++ app.js
@@ -1,29 +1,29 @@
 // 載入 express 並建構應用程式伺服器
-const express = require('express')
+const express = require("express");
 // // 載入 mongoose
-// const mongoose = require('mongoose') 
+// const mongoose = require('mongoose')
 // 載入 handlebars
-const exphbs = require('express-handlebars')
+const exphbs = require("express-handlebars");
 // 載入 bodyParser
-const bodyParser = require('body-parser')
+const bodyParser = require("body-parser");
 // 載入 method-override
-const methodOverride = require('method-override') 
+const methodOverride = require("method-override");
 // // 載入 Todo model
-// const Todo = require('./models/todo') 
+// const Todo = require('./models/todo')
+
+// 新增載入 express-session
+const session = require("express-session");
 
 // 加入這段 code, 僅在非正式環境時, 使用 dotenv
-if (process.env.NODE_ENV !== 'production') {
-  require('dotenv').config()
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
 }
 
-
 // 引用路由器
-const routes = require('./routes')
-require('./config/mongoose')
+const routes = require("./routes");
+require("./config/mongoose");
 
-
-
-const app = express()
+const app = express();
 
 // // 設定連線到 mongoDB
 // // mongoose.connect(process.env.MONGODB_URI) //改成以下
@@ -39,22 +39,34 @@ const app = express()
 //   console.log('mongodb connected!')
 // })
 
-
-
 // 建立一個名為 hbs 的樣板引擎, 並傳入 exphbs 與相關參數
-app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
-// 啟用樣板引擎 hbs 
-app.set('view engine', 'hbs')
+app.engine("hbs", exphbs({ defaultLayout: "main", extname: ".hbs" }));
+// 啟用樣板引擎 hbs
+app.set("view engine", "hbs");
+
+// 新增以下 session
+app.use(
+  session({
+    // secret 這個參數是 session 用來驗證 session id 的字串。
+    secret: "ThisIsMySecret",
+    // 當設定為 true 時，會在每一次與使用者互動後，
+    // 強制把 session 更新到 session store 裡。
+    resave: false,
+    // 強制將未初始化的 session 存回 session store。
+    // 未初始化表示這個 session 是新的而且沒有被修改過，
+    // 例如未登入的使用者的 session。
+    saveUninitialized: true,
+  })
+);
 
 // 用 app.use 規定每一筆請求都需要透過 body-parser 進行前置處理
-app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.urlencoded({ extended: true }));
 
 // 設定每一筆請求都會透過 methodOverride 進行前置處理
-app.use(methodOverride('_method'))
-
+app.use(methodOverride("_method"));
 
 // 將 request 導入路由器
-app.use(routes)
+app.use(routes);
 
 // 設定首頁路由
 // app.get('/', (req, res) => {
@@ -66,19 +78,18 @@ app.use(routes)
 // app.get('/', (req, res) => {
 //     // res.render('index')
 //     // 再次修改成以下
-//     Todo.find() 
+//     Todo.find()
 //     // 取出 Todo model 裡的所有資料 //從資料庫查找資料
-//     .lean() 
+//     .lean()
 //     .sort({ _id: 'asc' }) // 新增這裡：根據 _id 升冪排序
-//     // 把 Mongoose 的 Model 物件轉換成乾淨的 JavaScript 資料陣列 
-//     //把資料轉成單純的JS物件 
+//     // 把 Mongoose 的 Model 物件轉換成乾淨的 JavaScript 資料陣列
+//     //把資料轉成單純的JS物件
 //     //撈資料以後想用 res.render()，要先用 .lean() 來處理
-//     .then(todos => res.render('index', { todos })) 
+//     .then(todos => res.render('index', { todos }))
 //     // 將資料傳給 index 樣板 //然後把資料送到前端樣板
-//     .catch(error => console.error(error)) 
+//     .catch(error => console.error(error))
 //     // 錯誤處理 //如果發生意外, 執行錯誤處理
 // })
-
 
 // app.get('/todos/new', (req, res) => {
 //    return res.render('new')
@@ -87,15 +98,15 @@ app.use(routes)
 // app.post('/todos', (req, res) => {
 //    // 從 req.body 拿出表單裡的 name 資料
 //    const name = req.body.name
-   
+
 //    // 作法二 (先產生物件實例,再把實例存入 Todo　)
 //    const todo = new Todo({ name })
 //    return todo.save()
-   
+
 //    // 作法一 (直接操作 Todo)
 //    // 存入資料庫
 // //    return Todo.create({ name })
-//       // 新增完成後導回首頁     
+//       // 新增完成後導回首頁
 //      .then(() => res.redirect('/'))
 //      .catch(error => console.log(error))
 // })
@@ -145,7 +156,6 @@ app.use(routes)
 //       .catch(error => console.log(error))
 //   })
 
-
 // 設定 port 3000
 // app.listen(3000, () => {
 //   console.log('App is running on http://localhost:3000')
@@ -154,9 +164,9 @@ app.use(routes)
 // 修改環境設定
 // app.js
 // 如果在 Heroku 環境則使用 process.env.PORT
-// 否則為本地環境，使用 3000 
-const PORT = process.env.PORT || 3000
+// 否則為本地環境，使用 3000
+const PORT = process.env.PORT || 3000;
 // 設定應用程式監聽的埠號
 app.listen(PORT, () => {
-  console.log(`App is running on http://localhost:${PORT}`)
-})
+  console.log(`App is running on http://localhost:${PORT}`);
+});
