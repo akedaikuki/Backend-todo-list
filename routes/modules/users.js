bfa6727426ec2bb7861f656a975a614aebe56efa
ikuki <akedaikuki@gmail.com>
2023-08-10 10:19:59 +0800

    feat: add user modules

---
+++ routes/modules/users.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const router = express.Router();
+
+// 路由設定清單，這裡我們先簡單加入一條「登入表單頁面」的路由
+router.get("/login", (req, res) => {
+  res.render("login");
+});
+
+// 匯出路由模組
+module.exports = router;
